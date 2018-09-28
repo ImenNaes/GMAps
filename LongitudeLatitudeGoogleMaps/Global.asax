4900a10544a4b087dbf6313fd98e0e13300fbd76
Imen Nrnaes <i.nrnaes@root.sa>
2018-09-28 11:12:09 +0100

    Add project files.

---
+++ LongitudeLatitudeGoogleMaps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LongitudeLatitudeGoogleMaps.MvcApplication" Language="C#" %>
---
+++ LongitudeLatitudeGoogleMaps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LongitudeLatitudeGoogleMaps.Startup))]
+namespace LongitudeLatitudeGoogleMaps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
